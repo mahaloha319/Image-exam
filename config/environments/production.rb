ea571c450892a3bf3888af2a42bddcaaa03e1862
Misa <mahaloha3.19@gmail.com>
2018-03-26 06:53:41 +0000

    modify letter_deliver

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'heroku-test-app-misa319.c9users.io:8080' }
+  config.action_mailer.delivery_method = :letter_opener
 end
