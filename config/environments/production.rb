478d97f519277ccf06d911652db705a75dbf381d
Misa <mahaloha3.19@gmail.com>
2018-03-26 12:38:34 +0000

    modify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,9 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
- 
-  
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  host = 'website.com'
   config.action_mailer.default_url_options = { host: 'heroku-test-app-misa319.c9users.io:8080' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
