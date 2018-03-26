2f91940bc01436f8b80aed0d04a1e88ba68b3a74
Misa <mahaloha3.19@gmail.com>
2018-03-26 12:54:58 +0000

    modify

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,8 +91,7 @@ Rails.application.configure do
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  host = 'website.com'
-  config.action_mailer.default_url_options = { host: host}
+  config.action_mailer.default_url_options = { host: 'heroku-test-app-misa319.c9users.io:8080' }
   ActionMailer::Base.delivery_method = :smtp
   ActionMailer::Base.smtp_settings =
   {
